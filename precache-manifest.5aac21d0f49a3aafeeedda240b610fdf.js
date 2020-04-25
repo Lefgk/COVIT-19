3c59e3edde80d1dc9913cf9f9d18754d57db7e62
Eleftherios Giakoumakis <lefgiak@gmail.com>
2020-04-25 20:13:39 +0100

    Updates

---
+++ precache-manifest.5aac21d0f49a3aafeeedda240b610fdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e695693d333cc237cf70830fc9a3601c",
+    "url": "/COVIT-19/index.html"
+  },
+  {
+    "revision": "46765a59562a1a2a3d81",
+    "url": "/COVIT-19/static/css/2.7f75bbe2.chunk.css"
+  },
+  {
+    "revision": "0d4c2ccb86c19f4893fd",
+    "url": "/COVIT-19/static/css/main.d9ecd77e.chunk.css"
+  },
+  {
+    "revision": "46765a59562a1a2a3d81",
+    "url": "/COVIT-19/static/js/2.09e7bc3b.chunk.js"
+  },
+  {
+    "revision": "ac5d9a2ccd3ba848587d2a460762f2f0",
+    "url": "/COVIT-19/static/js/2.09e7bc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4c2ccb86c19f4893fd",
+    "url": "/COVIT-19/static/js/main.33e7d947.chunk.js"
+  },
+  {
+    "revision": "dcc9b1c830fe2b9c56ce",
+    "url": "/COVIT-19/static/js/runtime-main.d60b67ee.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-deploy/precache-manifest.0a592a9302b12ef966eb6b0ac5536e3e.js"
+  "/COVIT-19/precache-manifest.5aac21d0f49a3aafeeedda240b610fdf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-deploy/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/COVIT-19/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
